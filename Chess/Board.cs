51f2d759d25d46924444c5a3e9b2978a0eedf31d
freakyAion <FreakyAion@yandex.ru>
2023-02-28 11:07:28 +0500

    Start

---
+++ Chess/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Board
+    {
+        private char[,] board = new char[8, 8];
+
+        public char[,] ChessBoard
+        {
+            get { return board; }
+            set { board = value; }
+        }
+
+        private List<Piece> pieces;
+
+        public List<Piece> Pieces
+        {
+            get { return pieces; }
+            set { pieces = value; }
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < Pieces.Count(); i++)
+            {
+                Pieces[i].SetColour();
+                Console.SetCursorPosition(Pieces[i].XCoordinate, Pieces[i].YCoordinate);
+                Console.Write(Pieces[i].DisplaySymbol);
+            }
+        }
+
+    }
+}
---
+++ Chess/Piece.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Piece
+    {
+        private int x;
+        private int y;
+
+        public int XCoordinate
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int YCoordinate
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        private int[,] attack = new int[16, 16];
+        private int[,] move = new int[16, 16];
+
+        public int[,] AttackRange
+        {
+            get { return attack; }
+            set { attack = value; }
+        }
+        public int[,] MoveRange
+        {
+            get { return move; }
+            set { move = value; }
+        }
+
+        private char display;
+
+        public char DisplaySymbol
+        {
+            get { return display; }
+            set { display = value; }
+        }
+
+        private bool team;
+
+        public bool Team
+        {
+            get { return team; }
+            set { team = value; }
+        }
+
+        public void SetColour()
+        {
+            if (Team == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+            }
+        }
+    }
+}
